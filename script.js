bc1432a7659300f161054e6f92e85c0885163818
Karolína Mráčková <139132541+karolinamrackova@users.noreply.github.com>
2023-08-16 20:18:57 +0200

    corrected all .jpg to .JPG
    apparently github is  super case sensitive lol

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-
-  let currentPhoto = 0;
-  let imagesData = [
+let currentPhoto = 0;
+let imagesData = [
     {
-        photo: 'img/1.jpg',
+        photo: 'img/1.JPG',
         title: 'La Manche',
         description: 'I took this picture very early in the morning on a ferry to the United Kingdom. You can almost feel the cold, fresh, salty air.'
     },
     {
-        photo: 'img/2.jpg',
+        photo: 'img/2.JPG',
         title: 'Beach in Brighton',
         description: 'I love beaches. This misty beach with colorful pebbles and cold sea had a special charm.'
     },
     {
-        photo: 'img/3.jpg',
+        photo: 'img/3.JPG',
         title: 'Crab Leg',
         description: 'I found this crab leg on a beach in Brighton. I thought it was kinda cool.'
     },
     {
-        photo: 'img/4.jpg',
+        photo: 'img/4.JPG',
         title: 'Seagull',
         description: 'These fellows were everywhere in Brighton. They were very rude and malicious, but still cute.'
     },
     {
-        photo: 'img/5.jpg',
+        photo: 'img/5.JPG',
         title: 'Lighthouse',
         description: 'I love lighthouses. This one looks tiny, but it was just the shore being super high. I wonder if somebody lives there.'
     },
     {
-        photo: 'img/6.jpg',
+        photo: 'img/6.JPG',
         title: 'The Home of Charles Dickens',
         description: 'I visited the house where Charles Dickens lived and wrote his significant works.'
     },
     {
-        photo: "img/7.jpg",
+        photo: "img/7.JPG",
         title: "London",
         description: "How can you even visit Britain without visiting London? Stunning architecture, of course, but I like the small towns better."
     },
     {
-        photo: "img/8.jpg",
+        photo: "img/8.JPG",
         title: "Westminster Abbey",
         description: "I really admire all the details and decorations on huge buildings like this."
     },
     {
-        photo: "img/9.jpg",
+        photo: "img/9.JPG",
         title: "Buckingham Palace",
         description: "Of course I had to visit the Buckingham palace. Too crowded for me, honestly. I would prefer the cosy, empty shores any day."
     }
-
 ];
 
 $('#photo').attr('src', imagesData[currentPhoto].photo);
